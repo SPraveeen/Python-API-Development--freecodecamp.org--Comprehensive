3e4a6ac7dab192bd73f88844ab88e48616de3608
Praveen <140099825+SPraveeen@users.noreply.github.com>
2025-04-23 22:54:28 +0530

    1:10:00

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app=FastAPI()
+
 #request get method url takes this first = '/'
 @app.get("/")
 def root():
@@ -10,3 +13,7 @@ def root():
 def get_posts():
     return{"data":"This is your posts"}
 
+@app.post("/createposts")
+def create_posts(payLoad:dict=Body(...)):
+    print(payLoad)
+    return {"message":"successfully create post"}
