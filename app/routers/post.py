fa8b0ea3f183a99b96e65240f77985136e476106
Praveen <140099825+SPraveeen@users.noreply.github.com>
2025-04-28 22:22:08 +0530

    7:00:00 hrs no error

---
+++ app/routers/post.py
@@ -0,0 +1,78 @@
+from fastapi import FastAPI,Response,status,HTTPException,Depends, APIRouter
+from sqlalchemy.orm import Session
+from typing_extensions import List
+from .. import models,schemas
+from ..database import get_db
+
+router=APIRouter(
+    prefix="/posts",
+    tags=['Posts'] 
+)
+
+#getting all post
+@router.get("/",response_model=List[schemas.Post])
+def get_posts(db:Session=Depends(get_db)):
+    posts=db.query(models.Post).all()
+    return posts
+
+#create post
+@router.post("/",status_code=status.HTTP_201_CREATED,response_model=schemas.Post)
+def create_posts(post:schemas.PostCreate,db:Session=Depends(get_db)):
+    #                (post.title,post.content,post.published))
+    # new_post=cursor.fetchone()
+    # conn.commit()
+    new_post=models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
+
+#getting individual post
+@router.get("/{id}",response_model=schemas.Post)
+def get_post(id:int,db:Session=Depends(get_db)):
+    # manually convert to int even if we gave int as paramenter it will default change to str
+    # cursor.execute("""SELECT * FROM posts WHERE id=%s""",(str(id),))
+    # post=cursor.fetchone()
+    post=db.query(models.Post).filter(models.Post.id==id).first()
+    print(post)
+
+    if not post:
+        #instead of below lines import http and then use that
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id: {id} was not found")
+    return post
+
+#to delete a post
+@router.delete("/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int,db:Session=Depends(get_db)):
+    #deleting post
+    #find index in the array that has req id
+    #my_post.pop(index)
+    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """,(str(id)))
+    # deleted_post=cursor.fetchone()
+    # conn.commit()
+    post= db.query(models.Post).filter(models.Post.id==id)
+
+    if post.first()==None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f'Post with id {id} doesnot exist')
+    post.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+#update post
+@router.put("/{id}",response_model=schemas.Post)
+def update_post(id:int, updated_post:schemas.PostCreate,db:Session=Depends(get_db)):
+    #                WHERE id = %s RETURNING * """,
+    #                (post.title,post.content,post.published,str(id)))
+    # updated_post=cursor.fetchone()
+    # conn.commit()
+    post_query=db.query(models.Post).filter(models.Post.id==id)
+    post=post_query.first()
+    if post==None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f'Post with id {id} doesnot exist')
+    post_query.update(updated_post.dict(),
+                      synchronize_session=False)
+    db.commit()
+    return post_query.first()
--- app/schemas.py
+++ app/schemas.py
@@ -25,6 +25,9 @@ class UserOut(BaseModel):
     email:EmailStr
     created_at: datetime
 
-
     class Config:
         orm_mode=True
+
+class UserLogin(BaseModel):
+    email:EmailStr
+    password:str
