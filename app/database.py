9733d39245f3a48625a40b304e969d6ec9b9b10b
Praveen <140099825+SPraveeen@users.noreply.github.com>
2025-05-07 09:52:56 +0530

    9:15:00:

--- app/database.py
+++ app/database.py
@@ -1,13 +1,18 @@
 from sqlalchemy import create_engine 
 from sqlalchemy.ext.declarative import declarative_base 
 from sqlalchemy.orm import sessionmaker
-
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 # syntax for db connection
 # SQLALCHEMY_DATABASE_URL='postgresql://<username>:<password>@<ip-address/hostname>/<database-name>'
 
 SQLALCHEMY_DATABASE_URL ='postgresql://postgres:123456789@localhost/fastapi'
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False,bind=engine)
+
 Base = declarative_base()
 
 def get_db():
@@ -16,3 +21,15 @@ def get_db():
         yield db
     finally:
         db.close()
+
+# connecting database
+# # while True:
+# try:
+#     conn=psycopg2.connect(host='localhost',database='fastapi',user='postgres',
+#                         password='123456789',cursor_factory=RealDictCursor)
+#     cursor=conn.cursor()
+#     print("Database connection was successfull !")
+# except Exception as error:
+#     print("Connecting to database failed")
+#     print("Error: ",error)
+#     time.sleep(2)
--- app/routers/post.py
+++ app/routers/post.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI,Response,status,HTTPException,Depends, APIRouter
 from sqlalchemy.orm import Session
-from typing_extensions import List
+from typing_extensions import List,Optional
 from .. import models,schemas,oauth2
 from ..database import get_db
 
@@ -13,9 +13,9 @@ router=APIRouter(
 @router.get("/", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db), 
                 current_user: int = Depends(oauth2.get_current_user),
-                limit:int=10,skip:int=0):
-    print(limit)
-    posts = db.query(models.Post).limit(limit).offset(skip).all()
+                limit:int=10,skip:int=0,search : Optional[str]=""):
+    print(search)
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
 #create post
