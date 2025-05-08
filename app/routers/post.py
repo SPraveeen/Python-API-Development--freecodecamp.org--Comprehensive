7be0bb5b64ddb785008f6cb86541a35ce88ec2ef
Praveen <140099825+SPraveeen@users.noreply.github.com>
2025-05-08 06:07:09 +0530

    10:27:00 error trying in windsurf

--- app/routers/post.py
+++ app/routers/post.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI,Response,status,HTTPException,Depends, APIRouter
 from sqlalchemy.orm import Session
 from typing_extensions import List,Optional
 from .. import models,schemas,oauth2
+from sqlalchemy import func
 from ..database import get_db
 
 router=APIRouter(
@@ -10,13 +11,18 @@ router=APIRouter(
 )
 
 #getting all post
-@router.get("/", response_model=List[schemas.Post])
+# @router.get("/", response_model=List[schemas.Post])
+@router.get("/", response_model=List[schemas.PostOut])
 def get_posts(db: Session = Depends(get_db), 
                 current_user: int = Depends(oauth2.get_current_user),
                 limit:int=10,skip:int=0,search : Optional[str]=""):
-    print(search)
+    # print(search)
     posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
-    return posts
+    
+    results=db.query(models.Post,func.count(models.Vote.post_id).label("votes")).join(models.Vote,models.Vote.post_id==models.Post.id,isouter=True).group_by(models.Post.id).all()
+    # print(results)
+
+    return results
 
 #create post
 @router.post("/",status_code=status.HTTP_201_CREATED,response_model=schemas.Post)
